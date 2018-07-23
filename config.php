7c15d9559ac7ae8b06f156398b5b809f40277adf
suijiafeng <suijiafeng@hotmail.com>
2018-07-23 09:10:18 +0800

    优化了部分布局，样式

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+define('ROOT_PART', Root_part());
+define('APIKEY', getenv("APIKEY"));
+define('GJ_CODE', getenv("GJ_CODE"));
+define('SITE_NAME',getenv("SITE_NAME"));
+define('TITLENAME', getenv("TITLENAME"));
+define('EN2DEKEY',getenv("EN2DEKEY"));
+define('EMAIL', getenv("EMAIL"));
+?>
